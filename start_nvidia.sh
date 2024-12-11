e1260403feb772a094b46b45cc028971195f03e5
Peter Do <dophatgl@gmail.com>
2024-12-11 18:30:25 +0700

    Add files via upload

---
+++ start_nvidia.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+reset
+
+./SRBMiner-MULTI --algorithm xelishashv2_pepew --pool stratum+tcp://fi.mining4people.com:4176 --wallet PKLutqy8tbiWW8wmaDx8g7pkkR2SWhpwkm --password x --cpu-threads 0 --disable-gpu 
